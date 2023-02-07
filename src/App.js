c9572c9048a9635259d1328d36f9650dac01191a
student programmer <tazer16179@gmail.com>
2023-02-07 19:59:41 +0300

    finaly commitn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import gsap from 'gsap'
+import { useEffect, useRef } from 'react';
 
 function App() {
+  const timeline = gsap.timeline({
+    repeat: false,
+    defaults:{ duration: 1, ease: 'easeInOut'}
+  })
+  const e1Ref = useRef()
+  const e2Ref = useRef()
+  const e3Ref = useRef()
+  const lineRef = useRef()
+  const data = [ 
+    {id: 1, year: 2007, name: 'event one', ref: e1Ref },
+    {id: 2, year: 2017, name: 'event two', ref: e2Ref },
+    {id: 3, year: 2022, name: 'event three', ref: e3Ref }
+  ]
+  useEffect(() => {
+    timeline
+    .from(e1Ref.current, {autoAlpha: 0, x:-100, duration:.5})
+    .from(e2Ref.current, {autoAlpha: 0, x:-100, duration:.5})
+    .from(e3Ref.current, {autoAlpha: 0, x:-100, duration:.5})
+    .to(e1Ref.current, {autoAlpha: 0, x:-100, duration:.5, delay:1})
+    .to(e2Ref.current, {autoAlpha: 0, x:-100, duration:.5})
+    .to(e3Ref.current, {autoAlpha: 0, x:-100, duration:.5})
+    .to(lineRef.current, {opacity: 0, duration:.1})
+
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainBox">
+      <div className='wrapper'>
+        {data.map(el =>{
+          return(<div key={el.id} ref={el.ref}>
+            <h1>{el.year}</h1>
+            <p>{el.name}</p>
+          </div>)
+        })}
+        
+        
+        </div>
+        <div className='line' ref={lineRef}></div>
+        
     </div>
   );
 }
